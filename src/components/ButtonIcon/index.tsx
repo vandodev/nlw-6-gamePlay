ac1b8454e7ac682467664cc3bae8024feb216864
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-21 22:48:51 -0300

    Passando propriedades personalizadas para o botão

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,17 +1,27 @@
 import React from "react";
-import { Text, Image, View, TouchableOpacity } from "react-native";
+import {
+  Text,
+  Image,
+  View,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
 
 import DiscordImg from "../../assets/discord.png";
 import { styles } from "./styles";
 
-export function ButtonIcon() {
+type Props = TouchableOpacityProps & {
+  title: string;
+};
+
+export function ButtonIcon({ title, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon} />
       </View>
 
-      <Text style={styles.title}>Entrar com discord</Text>
+      <Text style={styles.title}> {title}</Text>
     </TouchableOpacity>
   );
 }
--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,39 +1,37 @@
-import React from 'react';
-import { Text, View, Image, StatusBar } from 'react-native';
-import {styles} from './styles';
-import IllustrationImg from '../../assets/illustration.png';
-import { ButtonIcon } from '../../components/ButtonIcon';
+import React from "react";
+import { Text, View, Image, StatusBar } from "react-native";
+import { styles } from "./styles";
+import IllustrationImg from "../../assets/illustration.png";
+import { ButtonIcon } from "../../components/ButtonIcon";
 
-export  function SignIn() {
+export function SignIn() {
   return (
-    <View style={styles.container} >
+    <View style={styles.container}>
       <StatusBar
-       barStyle="light-content"
-       backgroundColor="transparent"
-       translucent
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
       />
       <Image
         source={IllustrationImg}
-        style={styles.image} 
+        style={styles.image}
         resizeMode="stretch"
-       />
+      />
 
-       <View style={styles.content} >
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {"\n"}
+          suas jogatinas{"\n"}
+          facilmente
+        </Text>
 
-          <Text style={styles.title} >
-            Organize {'\n'}
-             suas jogatinas{'\n'}
-              facilmente
-          </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {"\n"}
+          favoritos com seus amigo
+        </Text>
 
-           <Text style={styles.subtitle} >
-            Crie grupos para jogar seus games {'\n'}
-            favoritos com seus amigo
-           </Text>
-           <ButtonIcon />
-
-       </View>
-      
+        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+      </View>
     </View>
   );
 }
