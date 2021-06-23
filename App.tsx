4d38ec5d1ad85f9e4069b696a255137dfc665c76
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-23 00:02:09 -0300

    Criando estrutura da tela Home e Profile

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
   Rajdhani_700Bold,
 } from "@expo-google-fonts/rajdhani";
 import AppLoading from "expo-app-loading";
-import { SignIn } from "./src/screens/Signin";
+import { Home } from "./src/screens/Home";
 import { Background } from "./src/components/Background";
 
 export default function App() {
@@ -29,7 +29,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <SignIn />
+      <Home />
     </Background>
   );
 }
---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+import { Avatar } from "../Avatar";
+
+export function Profile() {
+  return (
+    <View style={styles.container}>
+      <Avatar urlImage="https://github.com/vandodev.png" />
+
+      <View>
+        <View style={styles.user}>
+          <Text style={styles.greeting}>Olá,</Text>
+
+          <Text style={styles.username}>Evandro</Text>
+        </View>
+
+        <Text style={styles.message}>Hoje é dia de vitória</Text>
+      </View>
+    </View>
+  );
+}
