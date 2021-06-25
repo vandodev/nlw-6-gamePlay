c9b7c96dbb43a98516c69898a79b56c7dcf2e8e7
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-25 00:51:13 -0300

    Botão de compartilhar estático

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { View } from "react-native";
 import { Background } from "../../components/Background";
+import { Fontisto } from "@expo/vector-icons";
+import { BorderlessButton } from "react-native-gesture-handler";
 import { Header } from "../../components/Header";
 import { styles } from "./styles";
+import { theme } from "../../global/styles/theme";
 
 export function AppointmentDetails() {
   return (
     <Background>
-      <Header title={"Detalhes"} />
+      <Header
+        title={"Detalhes"}
+        action={
+          <BorderlessButton>
+            <Fontisto name="share" size={24} color={theme.colors.primary} />
+          </BorderlessButton>
+        }
+      />
     </Background>
   );
 }
