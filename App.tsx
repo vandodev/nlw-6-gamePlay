4069871bfafa66cd48cf72b604d203615e37788e
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-25 00:01:08 -0300

    Trabalhando com google fonts

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {SignIn} from './src/screens/Signin';
+import React from "react";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+import AppLoading from "expo-app-loading";
+import { SignIn } from "./src/screens/Signin";
 
 export default function App() {
-  return (
-   <SignIn />
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return <SignIn />;
+}
