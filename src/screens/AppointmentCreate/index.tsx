b4f26db557bc47cb7afc78868b64c11835b7ccb5
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-27 01:19:43 -0300

    Criando área de seleção de servidor

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { View, Text } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
 import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
 
 import { CategorySelect } from "../../components/CategorySelect";
+import { GuildIcon } from "../../components/GuildIcon";
 
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
@@ -28,6 +31,27 @@ export function AppointmentCreate() {
         setCategory={setCategory}
         categorySelected={category}
       />
+
+      <View style={styles.form}>
+        <RectButton>
+          <View style={styles.select}>
+            {
+              // <View style={styles.image} />
+              <GuildIcon />
+            }
+
+            <View style={styles.selectBody}>
+              <Text style={styles.label}>Selecione um servidor</Text>
+            </View>
+
+            <Feather
+              name="chevron-right"
+              color={theme.colors.heading}
+              size={18}
+            />
+          </View>
+        </RectButton>
+      </View>
     </Background>
   );
 }
--- src/screens/AppointmentCreate/styles.ts
+++ src/screens/AppointmentCreate/styles.ts
@@ -10,4 +10,32 @@ export const styles = StyleSheet.create({
     fontFamily: theme.fonts.title700,
     color: theme.colors.heading,
   },
+  form: {
+    paddingHorizontal: 24,
+    marginTop: 32,
+  },
+  select: {
+    flexDirection: "row",
+    width: "100%",
+    height: 68,
+    borderColor: theme.colors.secondary50,
+    borderWidth: 1,
+    borderRadius: 8,
+    alignItems: "center",
+    paddingRight: 25,
+    overflow: "hidden",
+  },
+
+  selectBody: {
+    flex: 1,
+    alignItems: "center",
+  },
+  image: {
+    width: 64,
+    height: 68,
+    backgroundColor: theme.colors.secondary40,
+    borderColor: theme.colors.secondary50,
+    borderWidth: 1,
+    borderRadius: 8,
+  },
 });
