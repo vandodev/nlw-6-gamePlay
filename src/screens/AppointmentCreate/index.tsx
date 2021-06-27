aaba9132b0a9fed54284d76639ff3fab1fdfcd32
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-27 00:36:44 -0300

    Reaproveitando component CategorySelect

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
+
+import { CategorySelect } from "../../components/CategorySelect";
 
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
 
 export function AppointmentCreate() {
+  const [category, setCategory] = useState("");
   return (
     <Background>
       <Header title={"Agendar partida"} />
@@ -19,6 +22,12 @@ export function AppointmentCreate() {
       >
         Categoria
       </Text>
+
+      <CategorySelect
+        hasCheckBox
+        setCategory={setCategory}
+        categorySelected={category}
+      />
     </Background>
   );
 }
